61eb8a3731aa4e6295e7c8ecc88b74ee16099530
ParthMehta2004 <mehtapm2004@gmail.com>
2026-03-24 03:03:57 +0530

    Create embedder.py

---
+++ rag/embedder.py
@@ -0,0 +1,15 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def chunk_text(text, chunk_size=300):
+    words = text.split()
+    chunks = []
+
+    for i in range(0, len(words), chunk_size):
+        chunks.append(" ".join(words[i:i+chunk_size]))
+
+    return chunks
+
+def generate_embeddings(chunks):
+    return model.encode(chunks)
