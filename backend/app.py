c5dc80bab1b8193d7e11d6f5cb52e56fcf8ae435
ParthMehta2004 <mehtapm2004@gmail.com>
2026-03-24 03:09:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,11 @@ from rag.loader import extract_text_from_pdf
 from rag.embedder import chunk_text, generate_embeddings
 from rag.retriever import Retriever
 from llm.llm_client import generate_answer
+from visualization.logger import log_query
 import os
 
 app = FastAPI()
+
 
 documents = []
 for file in os.listdir("data/raw"):
@@ -13,12 +15,17 @@ for file in os.listdir("data/raw"):
         text = extract_text_from_pdf(os.path.join("data/raw", file))
         documents.append(text)
 
+# Chunking
 chunks = []
 for doc in documents:
     chunks.extend(chunk_text(doc))
 
+
 embeddings = generate_embeddings(chunks)
+
+
 retriever = Retriever(embeddings, chunks)
+
 
 @app.get("/")
 def home():
@@ -28,5 +35,10 @@ def home():
 def ask(question: str):
     results = retriever.search(question)
     context = "\n".join(results)
+
     answer = generate_answer(context, question)
+
+
+    log_query(question, answer)
+
     return {"answer": answer}
